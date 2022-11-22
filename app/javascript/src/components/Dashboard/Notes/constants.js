bb88884140126485f3d5720b4353a5d8b80ad9e9
abilash-sajeev <abilash.sajeev@bigbinary.com>
2022-11-22 16:17:03 +0530

    Created UI for adding new note

--- app/javascript/src/components/Dashboard/Notes/constants.js
+++ app/javascript/src/components/Dashboard/Notes/constants.js
@@ -1,13 +1,49 @@
+import { buildSelectOptions } from "utils";
 import * as yup from "yup";
 
 export const NOTES_FORM_INITIAL_FORM_VALUES = {
   title: "",
   description: "",
+  assignedContact: null,
+  tags: [],
 };
+
+export const CONTACTS_DATA = buildSelectOptions([
+  "Ronald Richards",
+  "Jacob Jones",
+  "Albert Borer",
+  "Eve Smith",
+]);
+
+export const TAGS_DATA = buildSelectOptions([
+  "Getting Started",
+  "Onboarding",
+  "User Flow",
+  "UX",
+  "Bugs",
+  "V2",
+]);
 
 export const NOTES_FORM_VALIDATION_SCHEMA = yup.object().shape({
   title: yup.string().required("Title is required"),
   description: yup.string().required("Description is required"),
+  assignedContact: yup
+    .object()
+    .shape({
+      label: yup.string().oneOf(CONTACTS_DATA.map(contact => contact.label)),
+      value: yup.string().oneOf(CONTACTS_DATA.map(contact => contact.value)),
+    })
+    .required("Assigned contact is required"),
+  tags: yup
+    .array()
+    .of(
+      yup.object().shape({
+        label: yup.string().oneOf(TAGS_DATA.map(tag => tag.label)),
+        value: yup.string().oneOf(TAGS_DATA.map(tag => tag.value)),
+      })
+    )
+    .min(1, "Atleast one tag is required")
+    .required("Tag is required"),
 });
 
 export const USER_NAME = "Oliver Smith";
--- app/javascript/src/utils/index.js
+++ app/javascript/src/utils/index.js
@@ -1,3 +1,16 @@
 import * as R from "ramda";
 
 export const isPresent = R.pipe(R.either(R.isNil, R.isEmpty), R.not);
+
+export const camelize = str =>
+  str
+    .replace(/[^a-z0-9]/gi, " ")
+    .toLowerCase()
+    .split(" ")
+    .map((word, index) =>
+      index === 0 ? word : word.charAt(0).toUpperCase() + word.slice(1)
+    )
+    .join("");
+
+export const buildSelectOptions = values =>
+  values.map(value => ({ label: value, value: camelize(value) }));
